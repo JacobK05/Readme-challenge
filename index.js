b84232f87e07bbc9979ea0b8c867783c9490e50c
Jacob kaufenberg <Jacob.kaufenberg@icloud.com>
2024-01-08 13:56:17 -0600

    finsished questions working on console logging information

--- index.js
+++ index.js
@@ -1,41 +1,53 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer')
+const fs = require("fs")
+const inquirer = require("inquirer")
+const path = require("path");
 // TODO: Create an array of questions for user input
-inquirer
-  .prompt([
-    {
-      input: "input",
-      title: 'What is the title of your Project?',
-      name: 'Project Name',
-    },
-    
-    {
-      input:'input',
-      description: 'Write a description of your project.',
-      name:'short description',
-    },
+const questions = [ 
+  {
+  input: "input",
+  title: 'What is the title of your Project?',
+  name: 'Project Name',
+},
 
-    {
-      input:'input',
-      installation:'Proved a description to instal your project.',
-      name: 'instal',
-    },
+{
+  input:'input',
+  description: 'Write a description of your project.',
+  name:'short description',
+},
 
-    {
-      type:'list',
-      choices:'[MIT License, Apache License 2.0, ISC License, BSD 3-Clause License ]',
-      license:'Which license did you use. Please select one',
-      name: 'License'
-    },
-    
-    {
+{
+  input:'input',
+  installation:'Proved a description to instal your project.',
+  name: 'instal',
+},
 
-    },
-    {
+{
+  type:'list',
+  name: 'License',
+  license:'Which license did you use. Please select one',
+  choices:'[MIT License, Apache License 2.0, ISC License, BSD 3-Clause License, None ]',
+},
 
-    },
+{
+  type:'input',
+  usage:'type how to use and your projects functionalities',
+  name:'usage',
+},
 
-    ])
+{
+  type:'input',
+  contributors: 'list all the persons that contributed to the project.',
+  name:'contributors'
+},
+
+{
+  type:'input',
+  Info:'Proved your git hub name and other personal information',
+  name:'info'
+},
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -45,3 +57,9 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
+
+
+
+
